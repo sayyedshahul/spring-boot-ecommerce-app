97e569d217c96bd5d0e46462d78795aa544034a4
Shahul Sayyed <sayyedshahul279@gmail.com>
2026-02-05 15:03:28 +0530

    Add sorting to GET category method results

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -24,8 +24,10 @@ public class CategoryController {
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories(
             @RequestParam(defaultValue = AppConstants.PAGE_SIZE) int pageSize,
-            @RequestParam(defaultValue = AppConstants.PAGE_NUMBER) int pageNumber){
-        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
+            @RequestParam(defaultValue = AppConstants.PAGE_NUMBER) int pageNumber,
+            @RequestParam(defaultValue = AppConstants.SORT_BY) String sortBy,
+            @RequestParam(defaultValue = AppConstants.SORT_ORDER) String sortOrder){
+        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize, sortBy, sortOrder);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 public interface CategoryService {
-    CategoryResponse getAllCategories(int pageNumber, int pageSize);
+    CategoryResponse getAllCategories(int pageNumber, int pageSize, String sortBy, String sortOrder);
     CategoryDTO createCategory(CategoryDTO category);
     CategoryDTO deleteCategory(Long categoryId);
     CategoryDTO updateCategory(CategoryDTO categoryDTO, long categoryId);
